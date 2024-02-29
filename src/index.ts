325f3a1909ebb48b52024b4cb51b47c507374d04
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-02-29 20:47:20 +0700

    add body parser cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import express, { Application } from 'express'
 import { routes } from './routes'
+import { logger } from './utils/logger'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app: Application = express()
 const port: number = 4000
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
+})
+
 routes(app)
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`))
+app.listen(port, () => logger.info(`Server is listening on port ${port}`))
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction, Router } from 'express'
+import { logger } from '../utils/logger'
 
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Success get product')
   res.status(200).send({
     status: true,
     statusCode: 200,
@@ -11,7 +13,15 @@ ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
         name: 'Sepatu Adidas',
         price: 500000
       }
-    ],
-    responseMessage: 'Success'
+    ]
+  })
+})
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Success add product')
+  res.status(200).send({
+    status: true,
+    statusCode: 200,
+    data: req.body
   })
 })
