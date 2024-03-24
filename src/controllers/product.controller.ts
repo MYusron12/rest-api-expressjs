477b55927bdf196f8f26991c1192b13214577b74
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-03-24 21:56:06 +0700

    change insert

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,59 +1,73 @@
 import { Request, Response } from 'express'
 import { createProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
+import { addProductToDB, getProductById, getProductFromDb } from '../services/product.service'
+import { v4 as uuidv4 } from 'uuid'
 
-export const createProduct = (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
+  req.body.product_id = uuidv4()
   const { error, value } = createProductValidation(req.body)
   if (error) {
     logger.error('ERR: product - create = ', error.details[0].message)
     return res.status(422).send({
       status: false,
       statusCode: 422,
-      message: error.details[0].message,
-      data: {}
+      message: error.details[0].message
     })
   }
-  logger.info('Success add product')
-  return res.status(200).send({
-    status: true,
-    statusCode: 200,
-    message: 'Success add product',
-    data: value
-  })
-}
-export const getProduct = (req: Request, res: Response) => {
-  const products = [
-    { name: 'Sepatu', price: 200000 },
-    { name: 'Kaos', price: 100000 }
-  ]
-  const {
-    params: { name }
-  } = req
-  if (name) {
-    const filterProduct = products.filter((product) => {
-      if (product.name === name) {
-        return product
-      }
+  try {
+    await addProductToDB(value)
+    logger.info('Success add product')
+    return res.status(201).send({
+      status: true,
+      statusCode: 201,
+      message: 'Success add product'
     })
-    if (filterProduct.length === 0) {
-      logger.info('Data not found')
+  } catch (error) {
+    logger.error('ERR: product - create = ', error)
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error
+    })
+  }
+}
+
+export const getProduct = async (req: Request, res: Response) => {
+  const {
+    params: { id }
+  } = req
+  if (id) {
+    const product = await getProductById(id)
+    if (product) {
+      logger.info('Success get product')
+      return res.status(200).send({
+        status: true,
+        statusCode: 200,
+        data: product
+      })
+    } else {
+      logger.info('Failed get product')
       return res.status(404).send({
-        status: false,
+        status: true,
         statusCode: 404,
+        message: 'Data Not Found',
         data: {}
       })
     }
+  } else {
+    const products: any = await getProductFromDb()
     logger.info('Success get product')
     return res.status(200).send({
       status: true,
       statusCode: 200,
-      data: filterProduct[0]
+      data: products
     })
   }
-  logger.info('Success get product')
-  return res.status(200).send({
-    status: true,
-    statusCode: 200,
-    data: products
-  })
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const {
+    params: { id }
+  } = req
 }
