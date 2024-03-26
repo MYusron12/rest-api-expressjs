3beb92a9eac661fd96f7e09dd6343c38fec81c58
supardiputra <93508603+pardiputram@users.noreply.github.com>
2024-03-26 20:19:46 +0700

    create read update delete

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express'
-import { createProductValidation } from '../validations/product.validation'
+import { createProductValidation, updateProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { addProductToDB, getProductById, getProductFromDb } from '../services/product.service'
+import {
+  addProductToDB,
+  deleteProductById,
+  getProductById,
+  getProductFromDb,
+  updateProductById
+} from '../services/product.service'
 import { v4 as uuidv4 } from 'uuid'
 
 export const createProduct = async (req: Request, res: Response) => {
@@ -70,4 +76,69 @@ export const updateProduct = async (req: Request, res: Response) => {
   const {
     params: { id }
   } = req
+  const { error, value } = updateProductValidation(req.body)
+  if (error) {
+    logger.error('ERR: product - update = ', error.details[0].message)
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error.details[0].message
+    })
+  }
+  try {
+    const result = await updateProductById(id, value)
+    if (result) {
+      logger.info('Success update product')
+      return res.status(200).send({
+        status: true,
+        statusCode: 200,
+        message: 'Success update product'
+      })
+    } else {
+      logger.info('Data not found')
+      return res.status(404).send({
+        status: false,
+        statusCode: 404,
+        message: 'Failed update product'
+      })
+    }
+  } catch (error) {
+    logger.error('ERR: product - update = ', error)
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error
+    })
+  }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const {
+    params: { id }
+  } = req
+  try {
+    const result = await deleteProductById(id)
+    if (result) {
+      logger.info('Success delete product')
+      return res.status(200).send({
+        status: true,
+        statusCode: 200,
+        message: 'Success delete product'
+      })
+    } else {
+      logger.info('Data not found')
+      return res.status(404).send({
+        status: false,
+        statusCode: 404,
+        message: 'Failed delete product'
+      })
+    }
+  } catch (error) {
+    logger.error('ERR: product - delete = ', error)
+    return res.status(422).send({
+      status: false,
+      statusCode: 422,
+      message: error
+    })
+  }
 }
