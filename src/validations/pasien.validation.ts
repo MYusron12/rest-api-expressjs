c49f9ea28201c420f037a0d6e487467cc289d9f5
muhammad.yusron@jadinpratama.com <93508603+pardiputram@users.noreply.github.com>
2024-08-15 00:08:32 +0700

    add service pasien

--- src/validations/pasien.validation.ts
+++ src/validations/pasien.validation.ts
@@ -5,16 +5,16 @@ export const createPasienValidation = (payload: PasienType) => {
   const schema = Joi.object({
     id_pasien: Joi.string().required(),
     nama_lengkap: Joi.string().required(),
-    tanggal_lahir: Joi.date().iso().required(),
-    jenis_kelamin: Joi.string().valid('Laki-laki', 'Perempuan').required(),
+    tanggal_lahir: Joi.date().required(),
+    jenis_kelamin: Joi.string().required(),
     alamat: Joi.string().required(),
-    nomor_telepon: Joi.string().required(), // Format telepon yang umum
+    nomor_telepon: Joi.string().required(), 
     email: Joi.string().email().required(),
     nomor_identitas: Joi.string().required(),
     riwayat_medis: Joi.string().allow('', null),
-    golongan_darah: Joi.string().valid('A', 'B', 'AB', 'O').allow('', null),
-    status_perkawinan: Joi.string().valid('Belum Menikah', 'Menikah', 'Cerai', 'Duda/Janda').allow('', null),
-    tanggal_pendaftaran: Joi.date().iso().required(),
+    golongan_darah: Joi.string().allow('', null),
+    status_perkawinan: Joi.string().allow('', null),
+    tanggal_pendaftaran: Joi.date().required(),
     dokter_utama: Joi.string().allow('', null),
     akun_asuransi: Joi.string().allow('', null)
   })
@@ -24,16 +24,16 @@ export const createPasienValidation = (payload: PasienType) => {
 export const updatePasienValidation = (payload: Partial<PasienType>) => {
   const schema = Joi.object({
     nama_lengkap: Joi.string().allow('', null),
-    tanggal_lahir: Joi.date().iso().allow('', null),
-    jenis_kelamin: Joi.string().valid('Laki-laki', 'Perempuan').allow('', null),
+    tanggal_lahir: Joi.date().allow('', null),
+    jenis_kelamin: Joi.string().allow('', null),
     alamat: Joi.string().allow('', null),
     nomor_telepon: Joi.string().allow('', null),
     email: Joi.string().email().allow('', null),
     nomor_identitas: Joi.string().allow('', null),
     riwayat_medis: Joi.string().allow('', null),
-    golongan_darah: Joi.string().valid('A', 'B', 'AB', 'O').allow('', null),
-    status_perkawinan: Joi.string().valid('Belum Menikah', 'Menikah', 'Cerai', 'Duda/Janda').allow('', null),
-    tanggal_pendaftaran: Joi.date().iso().allow('', null),
+    golongan_darah: Joi.string().allow('', null),
+    status_perkawinan: Joi.string().allow('', null),
+    tanggal_pendaftaran: Joi.date().allow('', null),
     dokter_utama: Joi.string().allow('', null),
     akun_asuransi: Joi.string().allow('', null)
   })
