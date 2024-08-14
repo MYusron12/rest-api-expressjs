173da8f006e544b86a5f791545b270b545d84617
muhammad.yusron@jadinpratama.com <93508603+pardiputram@users.noreply.github.com>
2024-08-14 22:32:42 +0700

    add service pasien

--- src/routes/pasien.route.ts
+++ src/routes/pasien.route.ts
@@ -11,6 +11,6 @@ export const PasienRouter: Router = Router()
 
 PasienRouter.get('/', getPasien) // Mendapatkan semua pasien
 PasienRouter.get('/:id', getPasien) // Mendapatkan pasien berdasarkan ID
-PasienRouter.post('/', requireAdmin, createPasien) // Menambahkan pasien baru
-PasienRouter.put('/:id', requireAdmin, updatePasien) // Memperbarui pasien berdasarkan ID
-PasienRouter.delete('/:id', requireAdmin, deletePasien) // Menghapus pasien berdasarkan ID
+PasienRouter.post('/', /* requireAdmin, */ createPasien) // Menambahkan pasien baru
+PasienRouter.put('/:id', /* requireAdmin, */ updatePasien) // Memperbarui pasien berdasarkan ID
+PasienRouter.delete('/:id', /* requireAdmin, */ deletePasien) // Menghapus pasien berdasarkan ID
